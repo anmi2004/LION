6780c184cb536afb632a9eb2a077c99c6a35c67d
anmi2004 <123346lv@gmail.com>
2025-02-05 16:06:04 +0800

    更新 LION 插件信息，修改描述、作者和 URL，调整版本号，更新翻译文件，优化 OCR 功能，修复屏幕捕获逻辑

--- addon/globalPlugins/lion/__init__.py
+++ addon/globalPlugins/lion/__init__.py
@@ -2,7 +2,6 @@ import globalPluginHandler
 import addonHandler
 import scriptHandler
 import api
-import screenBitmap
 import logHandler
 import gui
 import tones
@@ -16,12 +15,13 @@ import config
 import wx
 import locationHelper
 from. import lionGui
-from .PPOCR_api import GetOcrApi
+from.PPOCR_api import GetOcrApi
 from difflib import SequenceMatcher
-from .PIL import Image
+from.PIL import Image, ImageGrab
 from io import BytesIO
 import ctypes
 import os
+
 
 addonHandler.initTranslation()
 active = False
@@ -29,10 +29,11 @@ prevString = ""
 counter = 0
 # 获取当前脚本所在目录
 current_dir = os.path.dirname(os.path.abspath(__file__))
-# 构建PaddleOCR-json.exe的绝对路径
+# 构建PaddleOCR - json.exe的绝对路径
 ocr_path = os.path.join(current_dir, "PaddleOCR", "PaddleOCR-json.exe")
-# 初始化PaddleOCR-json识别器对象，传入引擎路径
+# 初始化PaddleOCR - json识别器对象，传入引擎路径
 ocr = GetOcrApi(ocr_path)
+
 confspec = {
     "cropUp": "integer(0, 100, default=0)",
     "cropLeft": "integer(0, 100, default=0)",
@@ -57,15 +58,15 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
     def createMenu(self):
         self.prefsMenu = gui.mainFrame.sysTrayIcon.menu.GetMenuItems()[0].GetSubMenu()
         self.lionSettingsItem = self.prefsMenu.Append(wx.ID_ANY,
-                                                   # Translators: name of the option in the menu.
-                                                   _("&LION Settings..."),
-                                                   # Translators: tooltip text for the menu item.
-                                                   _("modify OCR zone and interval"))
+                                                     # Translators: name of the option in the menu.
+                                                     _("&LION Settings..."),
+                                                     # Translators: tooltip text for the menu item.
+                                                     _("modify OCR zone and interval"))
         gui.mainFrame.sysTrayIcon.Bind(wx.EVT_MENU, self.onSettings, self.lionSettingsItem)
 
     def terminate(self):
         try:
-            self.prefsMenu.RemoveItem(self.lionSettingsItem)
+            self.prefsMenu.Remove(self.lionSettingsItem)
         except wx.PyDeadObjectError:
             pass
 
@@ -91,7 +92,7 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
     def script_ReadLiveOcr(self, gesture):
         global active
         if self.isScreenCurtainRunning() and not active:
-            ui.message(_("Please disable screen curtain before using Windows 10 OCR."))
+            ui.message(_("Please disable screen curtain before using OCR."))
             return
         tones.beep(222, 333)
         if active == False:
@@ -130,23 +131,27 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
     def OcrScreen(self):
         global prevString
         global counter
+        global ocr
         left, top, width, height = self.targets[config.conf["lion"]["target"]]
-        sb = screenBitmap.ScreenBitmap(width, height)
-        pixels = sb.captureImage(left, top, width, height)
-        # 将捕获的像素数据转换为适合PaddleOCR-json的格式，这里假设可以转换为字节流
-        img = Image.frombytes('RGB', (width, height), pixels)
-        buffered = BytesIO()
-        img.save(buffered, format="PNG")
-        imageBytes = buffered.getvalue()
-        res = ocr.runBytes(imageBytes)
-        # 检查状态码，如果状态码不是100则忽略相关内容
-        if res.get("status", 0)!= 100:
-            return
-        for line in res["data"]:
-            text = line["text"]
-            threshold = SequenceMatcher(None, prevString, text).ratio()
-            if threshold < config.conf['lion']['threshold'] and text!= "" and text!= "Play":
-                ui.message(text)
-                prevString = text
-            if counter > 9:
-                counter = 0
+        try:
+            # 使用PIL的ImageGrab截取屏幕
+            img = ImageGrab.grab(bbox=(left, top, left + width, top + height))
+            buffered = BytesIO()
+            img.save(buffered, format="PNG")
+            imageBytes = buffered.getvalue()
+            res = ocr.runBytes(imageBytes)
+            logHandler.log.info(f"OCR result: {res}")
+            if res.get("code", 0)!= 100:  # 修改为判断 code 字段
+                logHandler.log.warning(f"OCR engine returned non - success status: {res.get('code')}")
+                return
+            all_text = ""
+            for line in res["data"]:
+                text = line["text"]
+                all_text += text + " "
+            threshold = SequenceMatcher(None, prevString, all_text).ratio()
+            if threshold < config.conf['lion']['threshold'] and all_text.strip():
+                ui.message(all_text.strip())
+                prevString = all_text.strip()
+        except Exception as e:
+            logHandler.log.error(f"Error in OcrScreen: {e}")
+
--- buildVars.py
+++ buildVars.py
@@ -5,48 +5,48 @@
 
 
 # Since some strings in `addon_info` are translatable,
-# we need to include them in the .po files.
+# we need to include them in the.po files.
 # Gettext recognizes only strings given as parameters to the `_` function.
 # To avoid initializing translations in this module we simply roll our own "fake" `_` function
 # which returns whatever is given to it as an argument.
 def _(arg):
-	return arg
+    return arg
 
 
 # Add-on information variables
 addon_info = {
-	# add-on Name/identifier, internal for NVDA
-	"addon_name": "LION",
-	# Add-on summary, usually the user visible name of the addon.
-	# Translators: Summary for this add-on
-	# to be shown on installation and add-on information found in Add-ons Manager.
-	"addon_summary": _("LION"),
-	# Add-on description
-	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
-	"addon_description": _("""Description for the add-on.
-It can span multiple lines."""),
-	# version
-	"addon_version": "2",
-	# Author(s)
-	"addon_author": "name <name@domain.com>",
-	# URL for the add-on documentation support
-	"addon_url": None,
-	# URL for the add-on repository where the source code can be found
-	"addon_sourceURL": None,
-	# Documentation file name
-	"addon_docFileName": "readme.html",
-	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
-	"addon_minimumNVDAVersion": None,
-	# Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
-	"addon_lastTestedNVDAVersion": None,
-	# Add-on update channel (default is None, denoting stable releases,
-	# and for development releases, use "dev".)
-	# Do not change unless you know what you are doing!
-	"addon_updateChannel": None,
-	# Add-on license such as GPL 2
-	"addon_license": None,
-	# URL for the license document the ad-on is licensed under
-	"addon_licenseURL": None,
+    # add-on Name/identifier, internal for NVDA
+    "addon_name": "LION",
+    # Add-on summary, usually the user visible name of the addon.
+    # Translators: Summary for this add-on
+    # to be shown on installation and add-on information found in Add-ons Manager.
+    "addon_summary": _("LION"),
+    # Add-on description
+    # Translators: Long description to be shown for this add-on on add-on information from add-ons manager
+    "addon_description": _("""does live OCR for the whole screen, refreshing at a specified interval.
+    Toggle with NVDA+alt+n """,),
+    # version
+    "addon_version": "2",
+    # Author(s)
+    "addon_author": "Stefan Moisei <vortex37@gmail.com>, Cary-rowen <manchen_0528@outlook.com>, anmi2004 <123346lv@gmail.com>",
+    # URL for the add-on documentation support
+    "addon_url": "https://github.com/anmi2004/LION",
+    # URL for the add-on repository where the source code can be found
+    "addon_sourceURL": "https://github.com/anmi2004/LION",
+    # Documentation file name
+    "addon_docFileName": "readme.html",
+    # Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
+    "addon_minimumNVDAVersion": "2023.1",
+    # Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
+    "addon_lastTestedNVDAVersion": "2024.1",
+    # Add-on update channel (default is None, denoting stable releases,
+    # and for development releases, use "dev".)
+    # Do not change unless you know what you are doing!
+    "addon_updateChannel": None,
+    # Add-on license such as GPL 2
+    "addon_license": None,
+    # URL for the license document the ad-on is licensed under
+    "addon_licenseURL": None,
 }
 
 # Define the python files that are the sources of your add-on.
@@ -57,19 +57,19 @@ It can span multiple lines."""),
 # pythonSources = ["addon/globalPlugins/*.py"]
 # For more information on SCons Glob expressions please take a look at:
 # https://scons.org/doc/production/HTML/scons-user/apd.html
-pythonSources = ["addon/globalPlugins/lion"]
+pythonSources = ["addon/globalPlugins/lion/*.py"]
 
 # Files that contain strings for translation. Usually your python sources
 i18nSources = pythonSources + ["buildVars.py"]
 
-# Files that will be ignored when building the nvda-addon file
+# Files that will be ignored when building the nvda - addon file
 # Paths are relative to the addon directory, not to the root directory of your addon sources.
 excludedFiles = []
 
 # Base language for the NVDA add-on
 # If your add-on is written in a language other than english, modify this variable.
 # For example, set baseLanguage to "es" if your add-on is primarily written in spanish.
-# You must also edit .gitignore file to specify base language files to be ignored.
+# You must also edit.gitignore file to specify base language files to be ignored.
 baseLanguage = "en"
 
 # Markdown extensions for add-on documentation
@@ -97,3 +97,4 @@ brailleTables = {}
 # displayName (name of the speech dictionary  shown to users and translatable),
 # mandatory (True when always enabled, False when not.
 symbolDictionaries = {}
+
